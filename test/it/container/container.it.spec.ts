4c633625770dd7db4f6b278d3495b7780cf9a966
efforthye <efforthye@gmail.com>
2025-01-09 20:23:50 +0900

    feat: migrate test container from typeorm to prisma

---
+++ test/it/container/container.it.spec.ts
@@ -0,0 +1,17 @@
+import { getPrismaClient } from "../util";
+
+describe("Container Connection Test", () => {
+  const prisma = getPrismaClient();
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it("테스트 컨테이너 연결 확인", async () => {
+    // when
+    const result = await prisma.$queryRaw`SELECT 1+1 as result`;
+
+    // then
+    expect(Number(result[0]["result"])).toBe(2);  // BigInt를 Number로 변환
+  });
+});
